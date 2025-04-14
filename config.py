34ab2d67ed4e1a6d6ec5c7029a0c1aa6ff0d1b2f
MatijaE <matija.erzen1@gmail.com>
2025-04-14 18:56:16 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "moj_tajni_kljuc"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "livecard.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
